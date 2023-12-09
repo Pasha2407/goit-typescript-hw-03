65109ebbfa545e975a867f97d1c9f8b6626aaa3e
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2023-12-09 20:07:26 +0200

    task 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,36 @@
+class Key {
+    constructor(private signature: number = Math.floor(Math.random() * 99) + 1) {
+    }
+    getSignature(): number {
+        return this.signature; // signature = від 1 до 99
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+    constructor(protected key: Key = key) {
+    }
+    comeIn(tenant: Person): void {
+        if (this.door) this.tenants.push(tenant); // якщо двері відкриті то орендар заходить в будинок
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) this.door = true; // якщо ключі збігаються то двері відкриті
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +40,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
